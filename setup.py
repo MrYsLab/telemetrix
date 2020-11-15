b3962a19c3349e1829c7e23e51458e14da3f240d
MrYsLab <MisterYsLab@gmail.com>
2020-11-15 15:31:38 -0500

    Release 1.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=['telemetrix'],
     install_requires=['pyserial'],
 
-    version='1.1',
+    version='1.2',
     description="Remotely Control And Monitor Arduino-Core devices",
     long_description=long_description,
     long_description_content_type='text/markdown',
