805dddef650f83f113a614e2636e6f39188749c7
MrYsLab <MisterYsLab@gmail.com>
2022-02-15 18:24:00 -0500

    i2c read without writing the register

--- telemetrix/telemetrix.py
+++ telemetrix/telemetrix.py
@@ -525,29 +525,34 @@ class Telemetrix(threading.Thread):
         time.sleep(.5)
 
     def i2c_read(self, address, register, number_of_bytes,
-                 callback=None, i2c_port=0, write_register=True):
+                 callback=None, i2c_port=0,
+                 write_register=True):
         """
-        Read the specified number of bytes from the specified register for
-        the i2c device.
+        Read the specified number of bytes from the
+        specified register for the i2c device.
 
 
         :param address: i2c device address
 
-        :param register: i2c register (or None if no register selection is needed)
+        :param register: i2c register (or None if no register
+                                       selection is needed)
 
         :param number_of_bytes: number of bytes to be read
 
-        :param callback: Required callback function to report i2c data as a
-                   result of read command
+        :param callback: Required callback function to report
+                         i2c data as a result of read command
 
        :param i2c_port: 0 = default, 1 = secondary
 
-       :param write_register: If True, the register is written before read
+       :param write_register: If True, the register is written
+                                       before read
                               Else, the write is suppressed
 
 
         callback returns a data list:
-        [I2C_READ_REPORT, address, register, count of data bytes, data bytes, time-stamp]
+
+        [I2C_READ_REPORT, address, register, count of data bytes,
+         data bytes, time-stamp]
 
         """
 
@@ -560,9 +565,10 @@ class Telemetrix(threading.Thread):
                                       callback=None, i2c_port=0,
                                       write_register=True):
         """
-        Read the specified number of bytes from the specified register for
-        the i2c device. This restarts the transmission after the read. It is
-        required for some i2c devices such as the MMA8452Q accelerometer.
+        Read the specified number of bytes from the specified
+        register for the i2c device. This restarts the transmission
+        after the read. It is required for some i2c devices such as the MMA8452Q
+        accelerometer.
 
 
         :param address: i2c device address
@@ -572,8 +578,8 @@ class Telemetrix(threading.Thread):
 
         :param number_of_bytes: number of bytes to be read
 
-        :param callback: Required callback function to report i2c data as a
-                   result of read command
+        :param callback: Required callback function to report i2c
+                         data as a result of read command
 
        :param i2c_port: 0 = default 1 = secondary
 
@@ -584,7 +590,8 @@ class Telemetrix(threading.Thread):
 
         callback returns a data list:
 
-        [I2C_READ_REPORT, address, register, count of data bytes, data bytes, time-stamp]
+        [I2C_READ_REPORT, address, register, count of data bytes,
+         data bytes, time-stamp]
 
         """
 
