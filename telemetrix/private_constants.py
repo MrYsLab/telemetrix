df886fe054dd24b4cf98ee769e66e9cda3d9abfe
MrYsLab <MisterYsLab@gmail.com>
2024-03-26 12:16:13 -0400

    Issue #77 fix Creating a second callback for a DHT sensor

--- telemetrix/private_constants.py
+++ telemetrix/private_constants.py
@@ -103,7 +103,7 @@ class PrivateConstants:
     FEATURES = 20
     DEBUG_PRINT = 99
 
-    TELEMETRIX_VERSION = "1.38"
+    TELEMETRIX_VERSION = "1.39"
 
     # reporting control
     REPORTING_DISABLE_ALL = 0
