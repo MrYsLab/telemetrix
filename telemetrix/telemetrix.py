0b2fb78b9ea164d53cd10f7f72337c9b6e91d957
MrYsLab <MisterYsLab@gmail.com>
2024-05-11 14:19:36 -0400

    Issue #83 Correct i2c_read comments.

--- telemetrix/telemetrix.py
+++ telemetrix/telemetrix.py
@@ -558,8 +558,9 @@ class Telemetrix(threading.Thread):
 
         callback returns a data list:
 
-        [I2C_READ_REPORT, address, register, count of data bytes,
-         data bytes, time-stamp]
+        [I2C_READ_REPORT, i2c_port, number of bytes read, address, register,
+        bytes read..., time-stamp]
+
 
         """
 
@@ -597,8 +598,8 @@ class Telemetrix(threading.Thread):
 
         callback returns a data list:
 
-        [I2C_READ_REPORT, address, register, count of data bytes,
-         data bytes, time-stamp]
+        [I2C_READ_REPORT, i2c_port, number of bytes read, address, register,
+        bytes read..., time-stamp]
 
         """
 
