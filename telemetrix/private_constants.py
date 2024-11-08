ca2e87db37c4722c26961ad65fe11ff50bf9a1b9
MrYsLab <MisterYsLab@gmail.com>
2024-11-08 11:20:28 -0500

    Addresses issue #90

--- telemetrix/private_constants.py
+++ telemetrix/private_constants.py
@@ -103,7 +103,7 @@ class PrivateConstants:
     FEATURES = 20
     DEBUG_PRINT = 99
 
-    TELEMETRIX_VERSION = "1.42"
+    TELEMETRIX_VERSION = "1.43"
 
     # reporting control
     REPORTING_DISABLE_ALL = 0
